42551a01680bb1909c146f445573fb7e9696d52a
dang2303 <8duchuynh@gmail.com>
2024-03-04 23:09:54 +0700

    create angular project and import bootstrap, font-awesome

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _db;
+public UsersController(DataContext db)
+{
+    _db = db;
+}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+             var user = await _db.AppUsers.ToListAsync();
+            return user;
+} 
+            [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser (int id){
+            return await _db.AppUsers.FirstOrDefaultAsync(u=> u.Id == id);
+}
+}
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'hello';
+  users: any;
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => this.users == res,
+      error: (err) => console.log(err),
+      complete: () => console.log('Request has completed'),
+    });
+  }
+}
