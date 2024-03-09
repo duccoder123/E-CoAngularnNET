0662917f615c2f905b0b98fd072fee24a1d9b9d3
dang2303 <8duchuynh@gmail.com>
2024-03-09 15:40:38 +0700

    create nav bả and angular service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd0de2bdf170a9b34467a2cae2d3012d049901b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8804983e28b87399aef542942e4433cbd667b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
